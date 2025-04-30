ea0a0ccdc89278bf62f1e154e022ac09ce88990f
Jose Martins <jose.martins@festpan.com.br>
2025-04-30 17:52:18 -0300

    Ajuste nomenclatura

--- ProjetoRespostaAvaliacao/Formularios/Questionario/frmInicialQuestionario.Designer.cs
+++ ProjetoRespostaAvaliacao/Formularios/Questionario/frmInicialQuestionario.Designer.cs
@@ -38,7 +38,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 62);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Indentificados";
+            this.button2.Text = "Identificados";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
